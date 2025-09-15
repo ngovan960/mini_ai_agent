6fd4349fccbc408d31f0b57391d859e80a2175c3
Hieu <ngovan960@gmail.com>
2025-09-15 20:48:14 +0700

    lan 2

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.tools import tool
+from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def main():
-    print("Hello from project1!")
+    model = ChatGoogleGenerativeAI(temperature=0)
 
+    tools = []
+    agent_executor = create_react_agent(model, tools)
 
-if __name__ == "__main__":
-    main()
+    print("welcome! I'm your AI assistant, Type 'quit' to exit.")
+    print("You can ask me to perform calculations or chat with me.")
